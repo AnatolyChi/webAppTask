220252194e16e71d8a42dfcdffd0e6d16295792d
Tolya <soulless_beast@icloud.com>
2021-08-23 02:47:47 +0300

    added role

--- src/main/java/com/example/webapptask/bean/User.java
+++ src/main/java/com/example/webapptask/bean/User.java
@@ -16,9 +16,9 @@ public final class User implements Serializable {
     @Setter
     private String lastName;
     @Setter
-    private String age;
-    @Setter
     private String email;
+    @Setter
+    private String age;
     private String role;
     private Date dateRegistered;
 
@@ -27,5 +27,26 @@ public final class User implements Serializable {
         this.dateRegistered = dateRegistered;
     }
 
+    public User(String login, String role, Date dateRegistered) {
+        this.login = login;
+        this.role = role;
+        this.dateRegistered = dateRegistered;
+    }
 
+    // Почему в Edit Личной страницы ошибка при изменении локали
+    // Та же проблема и в открытии новости
+    // Изменить логику смены локали без или с referer
+
+    // Connection Pool сделать интерфейсом(хз как лучше)
+    // Выкачать видео Spring from iCloud
+    // Досмотреть видео по Spring GeekBrains
+
+    // Сделать новостям педжинацию до 10 новостей на странице
+    // + Неавторизованный пользователь может просматривать только саммари новости (brief).
+    // + Авторизованные пользователи могут просматривать список новостей и новости целиком.
+    // + Авторизованный пользователь имеет профиль, профиль можно редактировать.
+    // Администратор системы может добавлять новости, удалять, редактировать.
+    // Авторизованный пользователь может добавить новость в Понравившееся и просматривать их отдельным списком.
+
+    //Возможно реализация комментирования новости, возможна функциональность, когда авторизованный пользователь предлагает свою новость на публикацию, администратор ее рассматривает и утверждает или нет.
 }
